a9acf0376e9f612fca2f0ce762e31f6a41faf3ff
chetan.kawadkar04@gmail.com <chetan.kawadkar04@gmail.com>
2021-07-29 11:03:45 +0530

    first

---
+++ JAVA/src/com/abc/def/First.java
@@ -0,0 +1,11 @@
+package com.abc.def;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("abc");
+System.out.println("nnnnnnnn");
+	}
+
+}
